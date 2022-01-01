fa89d90fead32c169ad74f7b83335d6ac60055a9
HoanDo <sieunhannhenkutesn@gmail.com>
2022-01-01 15:01:50 +0700

    refactor: update url

--- server.py
+++ server.py
@@ -3,11 +3,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/uploads/<foldername>/<filename>')
+@app.route('/upload/<foldername>/<filename>')
 def send_image(foldername,filename):
     try:
        print(filename)
-       return send_from_directory("uploads/{}".format(foldername), filename)
+       return send_from_directory("upload/{}".format(foldername), filename)
     except:
        return 'Error'
 if __name__ == '__main__':
